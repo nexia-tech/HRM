a3a79f97cad747713194ae4c15c1b9affd60ade5
sameershiekh77 <muhammadsameer.css@gmail.com>
2024-10-23 21:59:35 +0500

    fixed media file issue

--- config/settings.py
+++ config/settings.py
@@ -146,7 +146,7 @@ AUTH_USER_MODEL = 'users.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-MEDIA_URL = "/config/media/"
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "config/media")
 
 
