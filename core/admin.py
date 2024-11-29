eba44d23960236107ef672f2b2bac0a81b7451d4
sameershiekh77 <muhammadsameer.css@gmail.com>
2024-11-29 22:09:12 +0500

    ips model created

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from core.models import ConfigurationModel
+from core.models import ConfigurationModel, Ips
 
 
 admin.site.register(ConfigurationModel)
+
+class IpsAdmin(admin.ModelAdmin):
+    list_display = ['name','ip','active','created_at']
+    search_fields =['name','ip',]
+    list_filter = ['active','created_at']
+
+
+admin.site.register(Ips, IpsAdmin)
