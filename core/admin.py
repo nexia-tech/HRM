641dc92f50c66710548175cb6a0751ffcd620616
sameershiekh77 <muhammadsameer.css@gmail.com>
2024-11-29 22:10:09 +0500

    import and export added

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from core.models import ConfigurationModel, Ips
+from import_export.admin import ImportExportModelAdmin
 
 
 admin.site.register(ConfigurationModel)
 
-class IpsAdmin(admin.ModelAdmin):
+class IpsAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     list_display = ['name','ip','active','created_at']
     search_fields =['name','ip',]
     list_filter = ['active','created_at']
